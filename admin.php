d23d7923d4cdfe97ede492c6e5b7b7471e49bea8
Sara Mohamed <smohamedamer4@gmail.com>
2021-10-01 16:31:22 +0200

    editing admin file

--- admin.php
+++ admin.php
@@ -7,6 +7,7 @@
     <script>
             function check(){
                 e=document.getElementById('e').value;
+                // e=document.getElementById('e').value;
                 p=document.getElementById('p').value;
                 
                 y= /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; 
